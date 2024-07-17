e4aa4096dc8ee5d646650e87001fd92690141794
Long123-coder <longnguyenvan1608@gmail.com>
2024-07-17 16:44:45 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.wlltibb.mongodb.net',
+    DB_USER: 'nguyenvannhatlong1608',
+    DB_PASS: 'Long16082003',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'KHOA_BI_MAT',
+    JWT_EXPIRES: '04575675565687', // in milliseconds
+    EMAIL_USER: 'nguyenlongkk10@gmail.com', // gmail service
+    EMAIL_PASS: 'nqgvpvhajjbgcfea'
+  };
+  module.exports = MyConstants;
